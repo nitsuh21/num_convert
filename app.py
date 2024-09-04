98ef6ed9a3d4edf075e02dac235f9d09b2bd9c06
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-09-04 16:36:50 +0300

    defining int32

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+#Definining int32
+INT32_MIN = -2**31
+INT32_MAX = 2**31 - 1
+
 @app.route('/process', methods=['POST'])
 def process_numbers():
     data = request.get_json()
-    if isinstance(data, list) and all(isinstance(i, int) for i in data):
+    if isinstance(data, list) and all(isinstance(i, int) and INT32_MIN <= i <= INT32_MAX for i in data):
         result = sum(data)
         return jsonify(result=result)
     return jsonify(error="Invalid input"), 400
